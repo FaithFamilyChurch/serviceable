3e5841220dad7c61cdaa1d2eb8e71607ac8c8860
musicmunky <musicmunky@gmail.com>
2016-11-22 13:24:12 -0500

    Update to fix code in serviceable.js - somehow got copied over wrong

--- app/assets/javascripts/serviceable.js
+++ app/assets/javascripts/serviceable.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
   $('.chat-notification-icon').click(function(){
     $('.chat-notification-icon').toggleClass('active');
@@ -5,28 +6,28 @@ $(document).ready(function(){
     $('.workspace').toggleClass('wide');
   });
 
-/*
+  $('.menu a').click(function(){
     $('.workspace').toggleClass('one');
     $('.workspace').toggleClass('two');
   });
-  });
 
-    $('.loading-overlay').fadeToggle("fast","linear");
-    $('.loader').fadeToggle("fast","linear");
-    $('.loader2').fadeToggle("fast","linear");
-    $('.test-loading-toggle').fadeToggle("fast","linear");
-  });
-  });
-
+  $('.test-loading-toggle').click(function(){
     $('.loading-overlay').fadeToggle("fast","linear");
     $('.loader').fadeToggle("fast","linear");
     $('.loader2').fadeToggle("fast","linear");
     $('.test-loading-toggle').fadeToggle("fast","linear");
   });
 
-*/
-});
+  $('.loading-overlay').click(function(){
+    $('.loading-overlay').fadeToggle("fast","linear");
+    $('.loader').fadeToggle("fast","linear");
+    $('.loader2').fadeToggle("fast","linear");
+    $('.test-loading-toggle').fadeToggle("fast","linear");
+  });
 
+//   conditional sizing
+
+// Adds and removes body class depending on screen width.
   function screenClass() {
     if($('.workspace').innerWidth() < 1175) {
       $('.panel1').addClass('scale-step-1');
@@ -39,6 +40,7 @@ $(document).ready(function(){
   screenClass();
 
   // And recheck if window gets resized.
-  $(window).on('resize',function(){
+  $(window).bind('resize',function(){
     screenClass();
   });
+});
