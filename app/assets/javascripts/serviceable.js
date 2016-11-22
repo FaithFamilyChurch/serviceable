28ff6c6f9228db5c34a67506b0dbe5ee1e02c5a1
musicmunky <musicmunky@gmail.com>
2016-11-22 13:27:18 -0500

    Update to fix scoping for function

--- app/assets/javascripts/serviceable.js
+++ app/assets/javascripts/serviceable.js
@@ -1,46 +1,44 @@
 
 $(document).ready(function(){
-  $('.chat-notification-icon').click(function(){
-    $('.chat-notification-icon').toggleClass('active');
-    $('.right-sidebar').toggleClass('hidden');
-    $('.workspace').toggleClass('wide');
-  });
+	$('.chat-notification-icon').click(function(){
+		$('.chat-notification-icon').toggleClass('active');
+		$('.right-sidebar').toggleClass('hidden');
+		$('.workspace').toggleClass('wide');
+	});
 
-  $('.menu a').click(function(){
-    $('.workspace').toggleClass('one');
-    $('.workspace').toggleClass('two');
-  });
+	$('.menu a').click(function(){
+		$('.workspace').toggleClass('one');
+		$('.workspace').toggleClass('two');
+	});
 
-  $('.test-loading-toggle').click(function(){
-    $('.loading-overlay').fadeToggle("fast","linear");
-    $('.loader').fadeToggle("fast","linear");
-    $('.loader2').fadeToggle("fast","linear");
-    $('.test-loading-toggle').fadeToggle("fast","linear");
-  });
+	$('.test-loading-toggle').click(function(){
+		$('.loading-overlay').fadeToggle("fast","linear");
+		$('.loader').fadeToggle("fast","linear");
+		$('.loader2').fadeToggle("fast","linear");
+		$('.test-loading-toggle').fadeToggle("fast","linear");
+	});
 
-  $('.loading-overlay').click(function(){
-    $('.loading-overlay').fadeToggle("fast","linear");
-    $('.loader').fadeToggle("fast","linear");
-    $('.loader2').fadeToggle("fast","linear");
-    $('.test-loading-toggle').fadeToggle("fast","linear");
-  });
+	$('.loading-overlay').click(function(){
+		$('.loading-overlay').fadeToggle("fast","linear");
+		$('.loader').fadeToggle("fast","linear");
+		$('.loader2').fadeToggle("fast","linear");
+		$('.test-loading-toggle').fadeToggle("fast","linear");
+	});
 
-//   conditional sizing
+	screenClass();
+
+	// And recheck if window gets resized.
+	$(window).on('resize', function(){
+		screenClass();
+	});
+});
 
 // Adds and removes body class depending on screen width.
-  function screenClass() {
-    if($('.workspace').innerWidth() < 1175) {
-      $('.panel1').addClass('scale-step-1');
-    } else {
-      $('.panel1').removeClass('scale-step-1');
-    }
-  }
+function screenClass() {
+	if($('.workspace').innerWidth() < 1175) {
+		$('.panel1').addClass('scale-step-1');
+	} else {
+		$('.panel1').removeClass('scale-step-1');
+	}
+}
 
-  // Fire.
-  screenClass();
-
-  // And recheck if window gets resized.
-  $(window).bind('resize',function(){
-    screenClass();
-  });
-});
